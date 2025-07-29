34fd9d409115ffd33f1e3820a02c8abc5c72d8e2
Javiercord <72402479+funfely@users.noreply.github.com>
2025-07-29 17:54:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const productos = [
-  { id: 1, nombre: "Camiseta", precio: 3200, imagen: "/home/funfely/Documentos/CoderHouse/JS-comision73490/JS-flex-N2/imagenes/camisetas.png" },
-  { id: 2, nombre: "Pantalón", precio: 5100, imagen: "/home/funfely/Documentos/CoderHouse/JS-comision73490/JS-flex-N2/imagenes/Pantalones.png" },
-  { id: 3, nombre: "Zapatillas", precio: 9000, imagen: "/home/funfely/Documentos/CoderHouse/JS-comision73490/JS-flex-N2/imagenes/zapatillas.png" }
+  { id: 1, nombre: "Camiseta", precio: 3200, imagen: "imagenes/camisetas.png" },
+  { id: 2, nombre: "Pantalón", precio: 5100, imagen: "imagenes/Pantalones.png" },
+  { id: 3, nombre: "Zapatillas", precio: 9000, imagen: "imagenes/zapatillas.png" }
 ];
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
